e9a32b045ae35567c011f08dfe89f20dcb42be14
Emerson Mellado <emersonmellado@gmail.com>
2020-01-27 18:44:00 -0800

    Exercise 6 and 7

---
+++ js/index6.js
@@ -0,0 +1,11 @@
+var first_name = 'Jean-claude'
+var last_name = 'Van damme'
+var age = 30
+var dateOfBirth = '01/19/1990'
+var address = '123 Street Fight st.'
+
+console.log('First name:', first_name)
+console.log('Last Name:', last_name)
+console.log('Age:', age)
+console.log('Date of Birth:', dateOfBirth)
+console.log('Address:', address)
---
+++ js/index7.js
@@ -0,0 +1,11 @@
+let first_name = 'Jean-claude'
+let last_name = 'Van damme'
+let age = 30
+let dateOfBirth = '01/19/1990'
+let address = '123 Street Fight st.'
+
+console.log('First name:', first_name)
+console.log('Last Name:', last_name)
+console.log('Age:', age)
+console.log('Date of Birth:', dateOfBirth)
+console.log('Address:', address)
