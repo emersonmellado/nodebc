c3e4762c1dbf35cfe0794ea710c46d35b16a2d17
Emerson Mellado <emersonmellado@gmail.com>
2020-01-27 18:55:44 -0800

    Adjusting ex 7 and adding 8

--- js/index8.js
+++ js/index8.js
@@ -2,11 +2,11 @@
 //restructuring existing computer code—changing 
 //the factoring—without changing its external behavior
 
-let first_name = 'Jean-claude'
-let last_name = 'Van damme'
-let age = 30
-let dateOfBirth = '01/19/1990'
-let address = '123 Street Fight st.'
+const first_name = 'Jean-claude'
+const last_name = 'Van damme'
+const age = 30
+const dateOfBirth = '01/19/1990'
+const address = '123 Street Fight st.'
 
 console.log('First name:', first_name)
 console.log('Last Name:', last_name)
